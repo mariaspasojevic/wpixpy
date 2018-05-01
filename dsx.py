d6e46d7a785098c438fad20b59c70fc2c7b67395
mariaspasojevic <mariaspasojevic@gmail.com>
2018-05-01 10:14:40 -0700

    Update dsx.py, change function name, convert SM coords to R_E

--- dsx.py
+++ dsx.py
@@ -1,20 +1,32 @@
 import pandas as pd
 import numpy as np
 import datetime
+import os
 import matplotlib.pyplot as plt
 
-def dsx_read_orbit( orbitfile ):
+def read_orbit( orbitfile ):
 	'''
-	df = dsx_read_orbit( orbitfile ):
+	df = read_orbit( orbitfile ):
 
 	Inputs:
 		orbitfile = full path to file of format like:
 								'DSX_17213000000_17213235959_EPH_00_L1.txt'
 	Outputs:
-		df = dataframe containing all variables from the emphemeris file
+		Tuple containing (status, df)
+		status = 1 if valid orbit dataframe returned	
+		orbit = dataframe containing all variables from the emphemeris file
 	'''
 
-	f = open(orbitfile,'r')
+	# Constants
+	R_E = 6371 		# Earth Radius in km
+
+	# check if file found
+	if not os.path.isfile( orbitfile ):
+		print('%s: inputfile %s not found' \
+					 % (inspect.currentframe().f_code.co_name, hdf_L0_filename) )
+		return (-1,0)
+	else:
+		f = open(orbitfile, 'r')
 
 	# Read 13 lines of header
 	for i in range(0,12):
@@ -26,21 +38,18 @@ def dsx_read_orbit( orbitfile ):
   # Read variable names
 	ephem = f.readline()
 	ephem = ephem[2:-1].split()
-
 	f.readline()
 
   # Line 2: space environment
   # Read variable names
 	space_env = f.readline()
 	space_env = space_env[2:-1].split()
-
 	f.readline()
 
   # Line 3: conjunctions
   # Read variable names
 	conjunct = f.readline()
 	conjunct = conjunct[2:-1].split()
-
 	f.readline()
 
   # Line 4: blank
@@ -58,13 +67,13 @@ def dsx_read_orbit( orbitfile ):
 	f.close()
 
   # Convert list of lists into dataframe using column names
-	df = pd.DataFrame(records, columns = ephem+space_env+conjunct)
+	orbit = pd.DataFrame(records, columns = ephem+space_env+conjunct)
 
 	# Combine two datetime columns, drop star column
-	df['YYYYDDD'] = df.apply(lambda x: \
+	orbit['YYYYDDD'] = orbit.apply(lambda x: \
 		datetime.datetime.strptime(x['YYYYDDD']+x['HHMMSS.UUU'], '%Y%j%H%M%S.%f'), axis=1)
-	df.rename(columns={'YYYYDDD': 'UT'}, inplace=True)
-	df.drop(['*','HHMMSS.UUU'], axis=1, inplace=True)
+	orbit.rename(columns={'YYYYDDD': 'UT'}, inplace=True)
+	orbit.drop(['*','HHMMSS.UUU'], axis=1, inplace=True)
 
 	# remove *, plus two date columns from ephem
 	ephem = ephem[3::]
@@ -80,29 +89,41 @@ def dsx_read_orbit( orbitfile ):
 	space_env = [x for x in space_env if x not in space_env_flags]
 	
 	# convert to correct types
-	df[ephem+space_env] = df[ephem+space_env].astype(float)
-	df[ephem_flags+space_env_flags+conjunct] = \
-				df[ephem_flags+space_env_flags+conjunct].astype(int)
-	df[ephem_num] = df[ephem_num].astype(int)
-	df[ephem_dce] = df[ephem_dce].astype(str)
+	orbit[ephem+space_env] = orbit[ephem+space_env].astype(float)
+	orbit[ephem_flags+space_env_flags+conjunct] = \
+				orbit[ephem_flags+space_env_flags+conjunct].astype(int)
+	orbit[ephem_num] = orbit[ephem_num].astype(int)
+	orbit[ephem_dce] = orbit[ephem_dce].astype(str)
 
-	return df
+	# Convert SM X, Y, Z from km to R_E
+	sm = ['SM_X','SM_Y', 'SM_Z']
+	orbit.loc[:,sm] = orbit[sm].apply( lambda x: x/R_E )
 
-# exec(open('dsx_orbit.py').read())
-orbitfile ='DSX_17213000000_17213235959_EPH_00_L1.txt'
-df = dsx_read_orbit(orbitfile)
+	return (1,orbit)
 
-plt.subplot(131)
+# exec(open('dsx.py').read())
+
+basepath = '/Users/mystical/Work/DSX/Data/Ephemeris/'
+orbitfile = basepath + 'DSX_17213000000_17213235959_EPH_00_L1.txt'
+df = read_orbit(orbitfile)[1]
+
+plt.subplot(221)
 plt.plot(df.SM_X, df.SM_Y)
-plt.axis('equal')
+plt.axis('equal'), plt.axis('square')
+plt.xlabel('X, R_E')
+plt.ylabel('Y, R_E')
 
-plt.subplot(132)
+plt.subplot(223)
 plt.plot(df.SM_X, df.SM_Z)
-plt.axis('equal')
+plt.axis('equal'), plt.axis('square')
+plt.xlabel('X, R_E')
+plt.ylabel('Z, R_E')
 
-plt.subplot(133)
+plt.subplot(224)
 plt.plot(df.SM_Y, df.SM_Z)
-plt.axis('equal')
+plt.axis('equal'), plt.axis('square')
+plt.xlabel('Y, R_E')
+plt.ylabel('Z, R_E')
 
 plt.show()
 		
